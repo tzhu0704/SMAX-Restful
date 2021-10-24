e1458be483ed505939b2ff5e7f097c375976b5cf
tzhu0704 <gnzhutan@outlook.com>
2021-10-24 10:04:32 +0800

    Startwebhook update

--- bin/startwebhook.sh
+++ bin/startwebhook.sh
@@ -1,4 +1,5 @@
 kill -9 `ps -ef|grep "SMAX-restful"|grep -v "grep"|awk '{print $2} '`
 sleep 2 
-echo "begin to start SMAX"
+echo "begin to start SMAX Webhook"
 nohup java -jar SMAX-restful.jar &
+echo "SMAX Webhook Started"
